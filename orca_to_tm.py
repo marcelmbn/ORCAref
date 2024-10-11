981baa61d40f91580ec9a26919d41df3dbc98a3f
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-10-11 20:28:33 +0200

    Mulliken shell population also working
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- orca_to_tm.py
+++ orca_to_tm.py
@@ -11,6 +11,161 @@ import argparse as ap
 import subprocess as sp
 
 
+class Population:
+    """
+    Class to store the population information.
+
+    Contains
+    - atom type: atom number (int) and atom symbol (str)
+    - Mulliken charge (float)
+    - s, p, d, f, g populations (float)
+    """
+
+    def __init__(self, atom: int):
+        self.atom = atom
+        self.mull_q: float | None = None
+        self.mull_p_s: float | None = None
+        self.mull_p_p: float | None = None
+        self.mull_p_d: float | None = None
+        self.mull_p_f: float | None = None
+        self.mull_p_g: float | None = None
+
+    def __str__(self):
+        returnstr = f"Atom: {self.atom}\n"
+        if self.mull_q is not None:
+            returnstr += f"Mulliken charge: {self.mull_q:8.5f}\n"
+        if self.mull_p_s is not None:
+            returnstr += f"Mulliken population 's': {self.mull_p_s:8.5f}\n"
+        if self.mull_p_p is not None:
+            returnstr += f"Mulliken population 'p': {self.mull_p_p:8.5f}\n"
+        if self.mull_p_d is not None:
+            returnstr += f"Mulliken population 'd': {self.mull_p_d:8.5f}\n"
+        if self.mull_p_f is not None:
+            returnstr += f"Mulliken population 'f': {self.mull_p_f:8.5f}\n"
+        if self.mull_p_g is not None:
+            returnstr += f"Mulliken population 'g': {self.mull_p_g:8.5f}\n"
+        return returnstr
+
+
+def parse_orca_mulliken_charges(
+    populations: list[Population], orca_output: str
+) -> list[Population]:
+    """
+    Parse the ORCA output file and extract the Mulliken population analysis.
+    """
+    # First parse the Mulliken atomic charges
+    # split the content by lines
+    lines = orca_output.split("\n")
+    # iterate over the lines
+    for i, line in enumerate(lines):
+        # check if the line contains the Mulliken atomic charges
+        if "MULLIKEN ATOMIC CHARGES" in line:
+            # iterate over the lines after the line with the header
+            for j in range(i + 2, len(lines)):
+                # check if the line is empty
+                if lines[j].strip() == "":
+                    break
+                if "Sum of atomic charges" in lines[j]:
+                    break
+                # split the line by ":"
+                atom, charge = lines[j].split(":")
+                atom_number: str | int
+                atom_number, atom_symbol = atom.split()
+                atom_number = int(atom_number)
+                # append the atom and charge to the list
+                populations[atom_number].mull_q = float(charge)
+    return populations
+
+
+def parse_mulliken_reduced_orbital_charges(
+    populations: list[Population], orca_output: str, nat: int
+) -> list[Population]:
+    """
+    Parse the ORCA output file and extract the Mulliken reduced orbital charges.
+
+    The part looks as follows:
+    ```
+    --------------------------------
+    MULLIKEN REDUCED ORBITAL CHARGES
+    --------------------------------
+      0 O s       :     1.733068  s :     1.733068
+          pz      :     1.625117  p :     4.757872
+          px      :     1.582040
+          py      :     1.550715
+          dz2     :     0.008794  d :     0.052880
+          dxz     :     0.007340
+          dyz     :     0.015704
+          dx2y2   :     0.010845
+          dxy     :     0.010195
+
+      1 C s       :     0.962153  s :     0.962153
+          pz      :     1.074845  p :     2.777703
+          px      :     0.782766
+          py      :     0.920092
+          dz2     :     0.020886  d :     0.232749
+          dxz     :     0.036019
+          dyz     :     0.070005
+          dx2y2   :     0.072115
+          dxy     :     0.033725
+
+      2 C s       :     1.020250  s :     1.020250
+          pz      :     1.065344  p :     3.120061
+          ...
+    ```
+    """
+    # First parse the Mulliken atomic charges
+    # split the content by lines
+    lines = orca_output.split("\n")
+    # iterate over the lines
+    for i, line in enumerate(lines):
+        # check if the line contains the Mulliken reduced orbital charges
+        if "MULLIKEN REDUCED ORBITAL CHARGES" in line:
+            # iterate over the lines after the line with the header
+            atom_number: str | int | None
+            for j in range(i + 2, len(lines)):
+                # check if the line is empty
+                last_atom = False
+                if "s :" in lines[j]:
+                    # split the line by ":"
+                    atom_information, _, mull_s = lines[j].split(":")
+                    atom_number, atom_symbol, _ = atom_information.split()
+                    atom_number = int(atom_number)
+                    if atom_number >= nat - 1:
+                        last_atom = True
+                    # append the atom and charge to the list
+                    populations[atom_number].mull_p_s = float(mull_s)
+                if "p :" in lines[j]:
+                    if atom_number is None or not isinstance(atom_number, int):
+                        raise ValueError("No atom number found.")
+                    # split the line by ":"
+                    _, _, mull_p = lines[j].split(":")
+                    populations[atom_number].mull_p_p = float(mull_p.strip())
+                if "d :" in lines[j]:
+                    if atom_number is None or not isinstance(atom_number, int):
+                        raise ValueError("No atom number found.")
+                    # split the line by ":"
+                    _, _, mull_d = lines[j].split(":")
+                    populations[atom_number].mull_p_d = float(mull_d.strip())
+                if "f :" in lines[j]:
+                    if atom_number is None or not isinstance(atom_number, int):
+                        raise ValueError("No atom number found.")
+                    # split the line by ":"
+                    _, _, mull_f = lines[j].split(":")
+                    populations[atom_number].mull_p_f = float(mull_f.strip())
+                if "g :" in lines[j]:
+                    if atom_number is None or not isinstance(atom_number, int):
+                        raise ValueError("No atom number found.")
+                    # split the line by ":"
+                    _, _, mull_g = lines[j].split(":")
+                    populations[atom_number].mull_p_g = float(mull_g.strip())
+                if lines[j].strip() == "":
+                    atom_number = None
+                    if last_atom:
+                        break
+
+    return populations
+
+
 def generate_orca_input(qvSZP_args: str):
     """
     Generate the ORCA input file from the given structure file.
@@ -20,13 +175,20 @@ def generate_orca_input(qvSZP_args: str):
     print(f"Running command-line call: {binary} {' '.join(arguments)}")
     # run command via subprocess, write the output to qvSZP.out and the error to qvSZP.err
     with open("qvSZP.out", "w") as out, open("qvSZP.err", "w") as err:
-        sp.run([binary] + arguments, stdout=out, stderr=err)
+        sp.run([binary] + arguments, stdout=out, stderr=err, check=True)
 
 
 def execute_orca(orca_input: Path) -> tuple[Path, Path]:
     """
     Execute ORCA with the generated input file.
     """
+
+    # check full path of the ORCA binary (which is in the PATH)
+    orca_path = sp.run(
+        ["which", "orca"], capture_output=True, text=True, check=True
+    ).stdout.strip()
+
+    print(f"ORCA path: {orca_path}")
 
     # check if the ORCA input file exists
     if not orca_input.is_file():
@@ -37,7 +199,7 @@ def execute_orca(orca_input: Path) -> tuple[Path, Path]:
     orca_error_file = Path("orca.err").resolve()
     print(f"Running ORCA with input file '{orca_input}'")
     with open(orca_output_file, "w") as out, open(orca_error_file, "w") as err:
-        sp.run(["orca", orca_input], stdout=out, stderr=err)
+        sp.run([orca_path, orca_input], stdout=out, stderr=err, check=True)
 
     return orca_output_file, orca_error_file
 
@@ -179,41 +341,23 @@ def convert_orca_output(orca_output_file: Path) -> str:
     with open(orca_output_file, "r") as orca_out:
         orca_content = orca_out.read()
 
-    mulliken = parse_orca_mulliken(orca_content)
+    # grep the number of atoms from this line:
+    # ```Number of atoms                             ...      9```
+    natoms = int(orca_content.split("Number of atoms")[1].split()[1])
+    print(f"Found {natoms} atoms.")
+
+    # set up a list of Population objects
+    populations = [Population(i + 1) for i in range(natoms)]
+
+    # mulliken_charges contains the Mulliken atomic charges with atom number as key and charge as value
+    populations = parse_orca_mulliken_charges(populations, orca_content)
+    populations = parse_mulliken_reduced_orbital_charges(
+        populations, orca_content, natoms
+    )
+    for pop in populations:
+        print(pop)
 
     return ""
-
-
-def parse_orca_mulliken(orca_output: str) -> dict[int, float]:
-    """
-    Parse the ORCA output file and extract the Mulliken population analysis.
-    """
-    # First parse the Mulliken atomic charges
-    mulliken_atomic_charges: dict[int, float] = {}
-    # split the content by lines
-    lines = orca_output.split("\n")
-    # iterate over the lines
-    for i, line in enumerate(lines):
-        # check if the line contains the Mulliken atomic charges
-        if "MULLIKEN ATOMIC CHARGES" in line:
-            # iterate over the lines after the line with the header
-            for j in range(i + 2, len(lines)):
-                # check if the line is empty
-                if lines[j].strip() == "":
-                    break
-                if "Sum of atomic charges" in lines[j]:
-                    break
-                # split the line by ":"
-                atom, charge = lines[j].split(":")
-                atom_number: str | int
-                atom_number, atom_symbol = atom.split()
-                atom_number = int(atom_number) + 1
-                # append the atom and charge to the list
-                mulliken_atomic_charges.update({atom_number: float(charge)})
-    nat = len(mulliken_atomic_charges)
-    print(f"Found {nat} atomic charges.")
-    print(mulliken_atomic_charges)
-    return mulliken_atomic_charges
 
 
 def main():
